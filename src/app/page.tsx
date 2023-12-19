87687d42928c390f84adc34960cde5d9c6b3def1
Wagner.Squadp <wagner.squadp@genial.com.vc>
2023-12-19 11:54:26 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Camera from '@/components/camera/page'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Camera />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/camera/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import React, { useEffect } from 'react'
+import styles from './page.module.css'
+
+const Camera = () => {
+
+    useEffect(() => {
+        let video: HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
+
+        if (navigator.mediaDevices.getUserMedia !== null) {
+          var options = {
+            video: true,
+          };
+          navigator.mediaDevices.getUserMedia(options)
+            .then((stream) => {
+                video.srcObject = stream;
+                video.play();
+                console.log(stream, "streaming");
+            })
+            .catch(e => {
+                console.log("background error : " + e.name);
+            });
+        }
+      });
+
+  return (
+    <div className={styles.cameraContainer}>
+          <video id="video" height="400" width="450" autoPlay></video>
+    </div>
+  )
+}
+
+export default Camera
