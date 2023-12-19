4e5dcfa79152b4fd2b0c5417d6c9717b2245aee6
Wagner.Squadp <wagner.squadp@genial.com.vc>
2023-12-19 12:40:46 -0300

    ajustando video

--- src/components/camera/page.tsx
+++ src/components/camera/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect } from 'react'
 import styles from './page.module.css'
+import Quagga from 'quagga'; // ES6
 
 const Camera = () => {
 
@@ -23,6 +24,29 @@ const Camera = () => {
                 console.log("background error : " + e.name);
             });
         }
+
+        Quagga.init({
+            inputStream : {
+              name : "Live",
+              type : "LiveStream",
+              target: document.querySelector('#video')    
+            },
+            decoder : {
+              readers : ["code_128_reader"]
+            }
+          }, function(err) {
+              if (err) {
+                  console.log(err);
+                  return
+              }
+              console.log("Initialization finished. Ready to start");
+              Quagga.start();
+          });
+
+          Quagga.onDetected(data => {
+            alert(data.codeResult.code);
+            console.log(data);
+          })
       });
 
   return (
